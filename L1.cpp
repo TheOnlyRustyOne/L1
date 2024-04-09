af63805545fb2c488910fec55f04aa919c65f759
TheOnlyRustyOne <165979114+TheOnlyRustyOne@users.noreply.github.com>
2024-04-08 22:36:23 -0500

    Update L1.cpp

--- L1.cpp
+++ L1.cpp
@@ -4,36 +4,56 @@
 
 using namespace std;
 
+string concat(string a, string b){
+	string res = a + b;
+	return res;
+}
+
+string generarSubcadena(string a,int inicio,int fin){
+	string res;
+	
+	for(int i=inicio;i<fin;i++){
+		res += a[i];
+	}
+	return res;
+}
+
 int main(){
-	int opc =0,pos,inicio,longitud;
+	int opc,pos,inicio,longitud;
 	string cad1,cad2,cad3,conCad;
 	char busCar;
 	do{
 		cout<<"\t---MENU---"<<endl;
-		cout<<"1.inserte una cadena, para calcular su longitud"<<endl;
-		cout<<"2.inserte tres cadenas, para contatenerlas y calcular la longitud de las tres"<<endl;
-		cout<<"3.inserte una cadena"<<endl;
-		cout<<"4.inserte una cadena"<<endl;
-		cout<<"5.inserte una cadena"<<endl;
+		cout<<"1.insertar una cadena para calcular su longitud"<<endl;
+		cout<<"2.insertar tres cadenas para contatenerlas y calcular la longitud de las tres"<<endl;
+		cout<<"3.insertar una cadena para buscar un caracter en la misma"<<endl;
+		cout<<"4.insertar una cadena para generar una subcadena de la misma"<<endl;
+		cout<<"5.insertar una cadena para agregarle otra cadena desde una posicion"<<endl;
 	    cout<<"6.salir"<<endl;
-	    cin>>opc;
+	    cout<<endl<<"- ";cin>>opc;
+	    if(cin.good()){
+		
         system("cls");
+        fflush(stdin);	
         
         switch(opc){
-	  	case 1: cout<<"inserte cadena: ";cin>>cad1;
-	  		    cout<<"la longitud de la cadena es: "<<cad1.length(); 
-	  		    system("pause");break;	    
-	  	case 2: cout<<"inserte la primera cadena: ";cin>>cad1;
-	  	        cout<<"inserte la segunda cadena: ";cin>>cad2;
-	  	        cout<<"inserte la tercera cadena: ";cin>>cad3;
+	  	case 1: 
+		        cout<<"inserte cadena: ";getline(cin,cad1);
+	  		    cout<<"la longitud de la cadena es: "<<cad1.length()<<endl; 
+	  		    system("pause");break;	  
+				    
+	  	case 2: 
+	  		    cout<<"inserte la primera cadena: ";getline(cin,cad1);
+	  	        cout<<"inserte la segunda cadena: ";getline(cin,cad2);
+	  	        cout<<"inserte la tercera cadena: ";getline(cin,cad3);
 	  	        conCad = cad1 + cad2 + cad3;
 	  	        cout<<"la concatenacion de las 3 cadenas es: "<<conCad<<endl;
-	  	        cout<<"la logitud es: "<<conCad.length();
+	  	        cout<<"la logitud es: "<<conCad.length()<<endl;
 	  	        system("pause");break;
-
-	  		
-	  	case 3: cout<<"inserte cadena: ";cin>>cad1;
-	  	        cout<<"iserte caracter por buscar: ";cin>>busCar;
+				
+	  	case 3: 
+	  		    cout<<"inserte cadena: ";getline(cin,cad1);
+	  	        cout<<"inserte caracter por buscar: ";cin>>busCar;
 	  	        
 	  	        pos = -1;
                 for (int i = 0; i < cad1.length(); ++i) {
@@ -44,41 +64,79 @@ int main(){
                }
 	  	        
 	  	        if (pos != -1) {
-	  	            cout<<"su caracter fue encontrado en la posicion "<<pos<<" de la cadena";
+	  	            cout<<"su caracter fue encontrado en la posicion "<<pos<<" de la cadena"<<endl;
 	  	        }
 	  	        else {
-	  	            cout<<"su caracter no fue encontrado en la cadena";
+	  	            cout<<"su caracter no fue encontrado en la cadena"<<endl;
 	  	        }
 	  	        system("pause");break;
-	  		
-	  	case 4: cout << "Ingrese una cadena: ";
-                cin >> cadena1;
+				  
+	  	case 4: 
+	  		     cout << "Ingrese una cadena: ";
+                getline(cin,cad1);
                 cout << "Ingrese la posición inicial: ";
                 cin >> inicio;
+                if(!cin.good()){
+	  		    	cout<<"inserte un valor numerico, regresando al menu"<<endl;
+                  	cin.clear();
+                	cin.ignore();
+                	system("pause");
+                	break;
+				  }
+                inicio--;
                 cout << "Ingrese la longitud de la subcadena: ";
                 cin >> longitud;
-                cout << "La subcadena es: " << generarSubcadena(cadena1, inicio, longitud) << endl;
-                break;
-	  		
-	  	case 5:  cout << "Ingrese una cadena: ";
-                cin >> cadena1;
-                string aReemplazar, reemplazo;
-                cout << "Ingrese la cadena a reemplazar: ";
-                cin >> aReemplazar;
-                cout << "Ingrese el reemplazo: ";
-                cin >> reemplazo;
-                cout << "La cadena resultante es: " << reemplazarCadena(cadena1, aReemplazar, reemplazo) << endl;
-                break;		
-	  		
+                if(!cin.good()){
+	  		    	cout<<"inserte un valor numerico, regresando al menu"<<endl;
+                  	cin.clear();
+                	cin.ignore();
+                	system("pause");
+                	break;
+				  }
+				if(inicio > longitud || inicio<0||longitud>cad1.length()){
+	  		    	cout<<"error, siga indicaciones e inserte valores validos"<<endl;
+					system("pause");break;
+				}
+				else{
+                cout << "La subcadena es: " << generarSubcadena(cad1, inicio, longitud) << endl;
+                system("pause");break;
+			    }
+	  	case 5: 		
+	  		    cout<<"inserte cadena: ";getline(cin,cad1);
+	  		    cout<<"inserte posicion a agregar nueva cadena: ";cin>>pos;
+	  		    if(!cin.good()){
+	  		    	cout<<"inserte un valor numerico, regresando al menu"<<endl;
+                  	cin.clear();
+                	cin.ignore();
+                	system("pause");
+                	break;
+				  }
+	  		    fflush(stdin);	
+	  		    cout<<"inserte cadena a añadir: ";getline(cin,cad2);
+	  		    if(pos>cad1.length()||pos<0){
+	  		    	cout<<"error, siga indicaciones e inserte valores validos"<<endl;
+					system("pause");break;
+				  }
+				else{
+				  cad3 = generarSubcadena(cad1,0,pos);
+				  cad3 = concat(cad3,cad2);
+				  cout<<"cadena original: "<<cad1;
+				  cout<<endl<<"nueva cadena: "<<cad3<<endl;	
+				  system("pause");break;
+				}  
+	  		    
 	   	case 6: break;
 	   		
 	   	default:cout<<"error"; break;	
 	}
-	    getch();
+}
+else{
+	cout<<"inserte un valor numerico"<<endl;
+	cin.clear();
+	cin.ignore();
+	system("pause");
+}
 	    system("cls");
-	}while(opc != 6);
-	
-	cout<<"gracias por (no) vernos";
-	
+	}while(opc != 6);	
 	return 0;
 }
